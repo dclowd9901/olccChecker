582dfd2d8619349a8c8d3f30f3ce0cc89e62dc6e
David Drew <david.ddrew@gmail.com>
2020-10-15 13:00:39 -0700

    Fixes jitter

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ import lookup from './lookup.js';
 
 const ONE_MINUTE = 60000;
 
-// const jitter = Math.floor(Math.random() * ONE_MINUTE * 10);
-const jitter = 1;
+const jitter = Math.floor(Math.random() * ONE_MINUTE * 10);
 console.log(`Jittering for ${jitter / ONE_MINUTE} minutes`);
 setTimeout(() => {
     ageVerification().then(() => {
