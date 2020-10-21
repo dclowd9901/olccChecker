a893c3d119b123a627e9bda78388f7dbcdc6cb9b
David Drew <david.ddrew@gmail.com>
2020-10-21 00:45:30 -0700

    Makes jitter more readable

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ dotenv.config({ path: `${__dirname}/.env`});
 const ONE_MINUTE = 60000;
 
 const jitter = Math.floor(Math.random() * ONE_MINUTE * 10);
-console.log(`Jittering for ${jitter / ONE_MINUTE} minutes`);
+console.log(`Jittering for ${Math.floor(jitter / ONE_MINUTE)} minutes ${Math.floor((jitter/ONE_MINUTE) % 1 * 60)} seconds`);
 setTimeout(() => {
     ageVerification().then(() => {
         lookup();
